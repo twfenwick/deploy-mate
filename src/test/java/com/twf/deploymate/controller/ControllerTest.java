4b7960c64e274d4b68bacd409fbc3b82adb12537
tim fenwick <twfenwick@gmail.com>
2026-06-27 11:31:45 -0400

    remove unused import

--- src/test/java/com/twf/deploymate/controller/ControllerTest.java
+++ src/test/java/com/twf/deploymate/controller/ControllerTest.java
@@ -2,8 +2,6 @@ package com.twf.deploymate.controller;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ControllerTest {
     Controller controller = new Controller();
 
