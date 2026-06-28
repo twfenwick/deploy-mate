08709c1f4c0506fc264bc79f4c7a6f7db9811b31
tim fenwick <twfenwick@gmail.com>
2026-06-28 01:47:10 -0400

    add terraform destroy plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,10 +10,10 @@ module "vpc" {
 
   azs             = ["us-east-1a"] #, "us-east-1b"]
   private_subnets = ["10.0.1.0/24"] #, "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24"] #, "10.0.102.0/24"]
+  # public_subnets  = ["10.0.101.0/24"] #, "10.0.102.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  # enable_nat_gateway = true
+  # enable_vpn_gateway = true
 
   tags = {
     Terraform = "true"
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,10 +1,18 @@
 terraform {
   required_version = ">=1.2"
-  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">=6.28.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "global-terraform-state-bucket-twf"
+    key            = "apps/deploy-mate/dev/terraform.tfstate"
+    region         = "us-east-1"
+    use_lockfile   = true
+    encrypt        = true
   }
 }
